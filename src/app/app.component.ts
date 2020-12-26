ed45aa98a5cfa8efa39f02a5848851176ef97a7f
Sayed Anwar Mulla <anwar.mulla@nihilent.com>
2020-12-26 21:58:47 +0530

    Reactive from with validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ReactiveFormsAgain';
+ employeeForm:FormGroup;
+ngOnInit()
+{
+  this.employeeForm=new FormGroup({
+    id:new FormControl("1",[Validators.required]),
+    email:new FormControl("anwar.mulla@nihilent.com",[Validators.required,Validators.email]),
+    designation:new FormControl("Software Engineer",[Validators.required]),
+    employeename:new FormControl("Anwar Mulla",[Validators.required])
+  });
+}
+public createEmployee()
+{
+  console.log(this.employeeForm.valid);
+console.log(this.employeeForm.value);
+}
 }
